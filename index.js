eadee103da322eca2d04747239acaa3eacf0848a
Gospel Chinyereugo (Codebug) <gospelokpara@gmail.com>
2020-10-30 07:20:17 +0100

    set up igbo-english words

---
+++ index.js
@@ -0,0 +1,18 @@
+const table = require('./convertObjToTable');
+const alphabet = require('./getAlphabet');
+
+const app = require('express')();
+
+const port = 8001;
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`App running and listening to port ${process.env.PORT || port}`)
+});
+
+app.get('/', (req, res) => {
+  res.send('Home')
+});
+
+app.get('/get/table', table.getTable);
+
+app.get('/get/:alphabet', alphabet.getAlphabet);
